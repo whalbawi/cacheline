ca2fb0f898be332a81556b6a2f05066f8a52f765
Wael Halbawi <waelhalbawi@gmail.com>
2022-02-15 15:33:51 -0800

    Introduce matrix partitioning
    This is a high-level construct that facilitates parallel processing of a
    matrix.

--- src/matrix.h
+++ src/matrix.h
@@ -2,28 +2,79 @@
 #define CACHELINE_MATRIX_H_
 
 #include <functional>
+#include <vector>
 
 namespace cacheline {
 
-template <typename T, int num_rows, int num_cols> class Matrix {
+template <typename T> class Matrix {
   public:
-    Matrix() {
+    class View {
+      public:
+        void visit(int i, int j, std::function<void(T)> fn) {
+            matrix.visit(row + i, col + j, fn);
+        }
+
+        const int num_rows;
+        const int num_cols;
+
+      private:
+        friend class Matrix<T>;
+        View(Matrix<T>& matrix, int row, int col, int num_rows, int num_cols)
+            : matrix(matrix), row(row), col(col), num_rows(num_rows), num_cols(num_cols) {}
+
+        const Matrix<T>& matrix;
+        const int row;
+        const int col;
+    };
+
+    Matrix(const int num_rows, const int num_cols) : num_rows(num_rows), num_cols(num_cols) {
+        arr = new T* [num_rows] {};
         for (int i = 0; i < num_rows; ++i) {
-            for (int j = 0; j < num_cols; ++j) {
-                arr[i][j] = 0;
-            }
+            arr[i] = new T[num_cols]{};
         }
     }
 
     void set(int i, int j, T val) {
         arr[i][j] = val;
     }
-    void visit(int i, int j, std::function <void(T t)> fn) const {
+
+    void visit(int i, int j, std::function<void(T)> fn) const {
         fn(arr[i][j]);
     };
 
+    View view(int row, int col, int num_rows, int num_cols) {
+        return View(*this, row, col, num_rows, num_cols);
+    }
+
+    std::vector<View> partition(int num_row_blocks, int num_col_blocks) {
+        std::vector<View> views{};
+        int rows_per_block = num_rows / num_row_blocks;
+        int rows_rem = num_rows % rows_per_block;
+        int cols_per_block = num_cols / num_col_blocks;
+        int num_rows_processed = 0;
+        for (int i = 0; i < num_row_blocks; ++i) {
+            int add_row = std::min(1, rows_rem);
+            rows_rem = std::max(0, rows_rem - 1);
+            int cols_rem = num_cols % cols_per_block;
+            int num_cols_processed = 0;
+            for (int j = 0; j < num_col_blocks; ++j) {
+                int add_col = std::min(1, cols_rem);
+                cols_rem = std::max(0, cols_rem - 1);
+                views.push_back(view(num_rows_processed,
+                                     num_cols_processed,
+                                     rows_per_block + add_row,
+                                     cols_per_block + add_col));
+            }
+            num_rows_processed = rows_per_block + add_row;
+        }
+
+        return views;
+    }
+
   private:
-    T arr[num_rows][num_cols];
+    T** arr;
+    const int num_rows;
+    const int num_cols;
 };
 
 } // namespace cacheline
--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -1,12 +1,14 @@
 #include "matrix.h"
 
+#include <thread>
+
 #include "gtest/gtest.h"
 
 class MatrixTest : public ::testing::Test {
   protected:
     static const int num_rows = 5;
     static const int num_cols = 5;
-    cacheline::Matrix<int, num_rows, num_cols> matrix;
+    cacheline::Matrix<int> matrix{num_rows, num_cols};
 };
 
 TEST_F(MatrixTest, Initialization) {
@@ -21,4 +23,66 @@ TEST_F(MatrixTest, Initialization) {
             matrix.visit(i, j, [i, j](int v) { ASSERT_EQ(v, i + j); });
         }
     }
+}
+
+TEST_F(MatrixTest, Partition) {
+    int rows_per_block = 50;
+    int num_row_blocks = 2;
+    int num_rows = num_row_blocks * rows_per_block;
+    int cols_per_block = 50;
+    int num_col_blocks = 4;
+    int num_cols = num_col_blocks * cols_per_block;
+
+    cacheline::Matrix<int> matrix{num_rows, num_cols};
+    for (int i = 0; i < num_rows; ++i) {
+        for (int j = 0; j < num_cols; ++j) {
+            matrix.set(i, j, 1);
+        }
+    }
+
+    std::vector<cacheline::Matrix<int>::View> blocks =
+        matrix.partition(num_row_blocks, num_col_blocks);
+    int sum = 0;
+    for (auto&& block : blocks) {
+        ASSERT_EQ(block.num_rows, rows_per_block);
+        ASSERT_EQ(block.num_cols, cols_per_block);
+        for (int i = 0; i < block.num_rows; ++i) {
+            for (int j = 0; j < block.num_cols; ++j) {
+                block.visit(i, j, [&](int val) { sum += val; });
+            }
+        }
+    }
+
+    ASSERT_EQ(sum, num_rows * num_cols);
+}
+
+TEST_F(MatrixTest, PartitionUneven) {
+    int num_row_blocks = 2;
+    int num_rows = 101;
+    int num_col_blocks = 4;
+    int num_cols = 17;
+
+    cacheline::Matrix<int> matrix{num_rows, num_cols};
+    for (int i = 0; i < num_rows; ++i) {
+        for (int j = 0; j < num_cols; ++j) {
+            matrix.set(i, j, 1);
+        }
+    }
+
+    std::vector<cacheline::Matrix<int>::View> blocks =
+        matrix.partition(num_row_blocks, num_col_blocks);
+    int sum = 0;
+    for (auto&& block : blocks) {
+        ASSERT_GE(block.num_rows, num_rows / num_row_blocks);
+        ASSERT_LE(block.num_rows, num_rows / num_row_blocks + 1);
+        ASSERT_GE(block.num_cols, num_cols / num_col_blocks);
+        ASSERT_LE(block.num_cols, num_cols / num_col_blocks + 1);
+        for (int i = 0; i < block.num_rows; ++i) {
+            for (int j = 0; j < block.num_cols; ++j) {
+                block.visit(i, j, [&](int val) { sum += val; });
+            }
+        }
+    }
+
+    ASSERT_EQ(sum, num_rows * num_cols);
 }
