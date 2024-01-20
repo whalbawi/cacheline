2afc97954894fe9e8768d24d909cad071193452a
Wael Halbawi <waelhalbawi@gmail.com>
2024-01-20 12:35:06 -0800

    test

--- benchmark/counters_bench.cpp
+++ benchmark/counters_bench.cpp
@@ -1,19 +1,23 @@
 #include <atomic>
+#include <numeric>
 
 #include "benchmark/benchmark.h"
 
-template <typename T> struct alignas(64) CounterAligned { T data; };
+template <typename T> struct alignas(64) CounterAligned {
+    alignas(64) T data;
+};
 
 template <typename T> struct Counter { T data; };
 
-int num_increments = 1 * 1024;
+int num_increments = 1000000;
 
 template <typename T> void BM_increment(benchmark::State& state) {
     static T* counters;
     if (state.thread_index() == 0) {
-        counters = new T[state.threads()];
+        counters = new T[state.threads()]{};
     }
 
+    benchmark::DoNotOptimize(counters);
     for (auto _ : state) {
         for (int i = 0; i < num_increments; ++i) {
             counters[state.thread_index()].data++;
@@ -22,14 +26,18 @@ template <typename T> void BM_increment(benchmark::State& state) {
     }
 
     if (state.thread_index() == 0) {
+        long sum = 0;
+        for (int i = 0; i < state.threads(); ++i) {
+            sum += counters[i].data;
+        }
         delete[] counters;
+        state.counters["sum"] = sum;
     }
 }
 
-BENCHMARK(BM_increment<Counter<long>>)->DenseThreadRange(1, 8, 1);
+#define COUNTER_BENCHMARK(x) BENCHMARK(BM_increment<x>)->DenseThreadRange(1, 16, 1);
 
-BENCHMARK(BM_increment<CounterAligned<long>>)->DenseThreadRange(1, 8, 1);
-
-BENCHMARK(BM_increment<Counter<std::atomic<long>>>)->DenseThreadRange(1, 8, 1);
-
-BENCHMARK(BM_increment<CounterAligned<std::atomic<long>>>)->DenseThreadRange(1, 8, 1);
+COUNTER_BENCHMARK(Counter<int64_t>);
+COUNTER_BENCHMARK(CounterAligned<int64_t>);
+// COUNTER_BENCHMARK(Counter<std::atomic<long>>);
+// COUNTER_BENCHMARK(CounterAligned<std::atomic<long>>);
--- src/cacheline.cpp
+++ src/cacheline.cpp
@@ -1,9 +1,13 @@
 #include "cacheline.h"
+#include <fstream>
 
 #include "version.h"
 
 namespace cacheline {
 
+std::fstream file;
+std::filebuf buf;
+void function() {}
 std::string Cacheline::get_version() {
     return "Version: " + version() + " - Commit: " + git_commit_hash();
 }
